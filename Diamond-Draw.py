d1cef5bb3bb50531ed5b909a28d04d2b08620491
Hamad Allo <hamadallo@users.noreply.github.com>
2017-10-21 03:48:18 +0300

    Add files via upload
    My gift to a programming master, Dr. Moussa

---
+++ Diamond-Draw.py
@@ -0,0 +1,14 @@
+
+# My gift to a programming master, Dr. Moussa
+
+i=1
+for i in range(8):
+    print ("-"*(12-i) + "*"*(0+i) + "*"*(1+i) + "-"*(12-i) )
+
+print ("******* Dr.Moussa *******")
+
+x=1
+for x in range(8):
+    print ("-"*(x+5) + "*"*(7-x) + "*"*(8-x) + "-"*(x+5) )
+    x=x-2
+
